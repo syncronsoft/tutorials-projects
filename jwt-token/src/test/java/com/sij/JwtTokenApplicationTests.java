f3d2e7c6bc883d0c538164fbe7693d5aaf49a6b6
Avinash Dubey <syncronsoft@gmail.com>
2024-12-29 11:28:45 +0530

    Initial commit

---
+++ jwt-token/src/test/java/com/sij/JwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
